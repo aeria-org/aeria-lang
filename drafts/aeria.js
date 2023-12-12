44d43b1f889cfdbd14d18ff9193bc4b0071c5e4e
ringeringeraja <joaosan177@gmail.com>
2023-12-12 03:44:16 -0300

    add js output

---
+++ drafts/aeria.js
@@ -0,0 +1,45 @@
+exports.person = () => ({
+  description: {
+    $id: 'person',
+    properties: {
+      first_name: {
+        type: 'string'
+      },
+      last_name: {
+        type: 'string'
+      },
+      age: {
+        type: 'number'
+      },
+      responsible: {
+        $ref: 'person',
+        indexes: [
+          'first_name',
+          'last_name'
+        ]
+      },
+      friends: {
+        type: 'array',
+        items: {
+          $ref: 'person',
+          indexes: [
+            'first_name',
+            'last_name'
+          ],
+          constrants: {
+            not: {
+              operator: 'equal',
+              term1: 'blocked',
+              term2: false
+            }
+          }
+        }
+      }
+    },
+    table: [
+      'first_name',
+      'last_name',
+      'age'
+    ]
+  }
+})
