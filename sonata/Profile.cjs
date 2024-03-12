d4ece5353e13db39ea920724f96e680639cc503b
ringeringeraja <joaosan177@gmail.com>
2024-03-11 21:53:58 -0300

    cleanup

--- sonata/Profile.cjs
+++
@@ -1,5 +0,0 @@
-import {defineCollection,deepMerge,} from "sonata-api"
-export const Profile = (description,) => defineCollection(deepMerge({description:{$id:"Profile",properties:{first_name:{type:"string",},last_name:{type:"string",},profile_picture:{$ref:"File",accept:["image/*",],},name:{getter:(values) => {
-        return `${values.first_name} ${values.last_name}`;
-      }
-      ,},},},},description,),)
--- sonata/Profile.js
+++
@@ -1,5 +0,0 @@
-const {defineCollection,deepMerge,} = require("sonata-api")
-exports.Profile = (description,) => defineCollection(deepMerge({description:{$id:"Profile",properties:{first_name:{type:"string",},last_name:{type:"string",},profile_picture:{$ref:"File",accept:["image/*",],},name:{getter:(values) => {
-        return `${values.first_name} ${values.last_name}`;
-      }
-      ,},},},},description,),)
