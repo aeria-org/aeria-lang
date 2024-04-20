a178fd272d7339d5edf2e356837fe8fb5fc8ed07
ringeringeraja <joaosan177@gmail.com>
2024-04-20 20:41:18 -0300

    update playground

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,6 +1,6 @@
 import { init, createRouter } from 'aeria'
-import { person } from 'aeria-runtime/person'
-// import { pet } from 'aeria-runtime/pet'
+import { person } from 'aeria-runtime/collections/person'
+import { pet } from 'aeria-runtime/collections/pet'
 
 export const router = createRouter()
 
