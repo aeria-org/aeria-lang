abb639a1b84e3c0e6dc063b2df08b2dd80658830
ringeringeraja <joaosan177@gmail.com>
2024-04-20 18:09:57 -0300

    playground

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,19 @@
-import { init } from 'aeria'
+import { init, createRouter } from 'aeria'
+import { person } from 'aeria-runtime/person'
+// import { pet } from 'aeria-runtime/pet'
+
+export const router = createRouter()
+
+router.GET('/test', async (context) => {
+  const person = await context.collections.person.model.findOne()
+  if( person ) {
+    //
+  }
+})
 
 export default init({
+  collections: {
+    person,
+    // pet,
+  }
 })
