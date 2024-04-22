3e26ed9242c7e5f8fb9b9fc1c6cbb8c9b89b4561
ringeringeraja <joaosan177@gmail.com>
2024-04-21 22:08:22 -0300

    update playground

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,19 +1,30 @@
 import { init, createRouter } from 'aeria'
+export * from 'aeria-runtime/collections/pet'
 import { person } from 'aeria-runtime/collections/person'
 import { pet } from 'aeria-runtime/collections/pet'
 
 export const router = createRouter()
 
+export const collections = {
+  person,
+  pet,
+}
+
 router.GET('/test', async (context) => {
   const person = await context.collections.person.model.findOne()
+  const pet = await context.collections.pet.model.findOne()
+
   if( person ) {
-    //
+    person.options.friends[0].type
+  }
+
+
+  if( pet ) {
+    pet.breed === 'dog'
+    // @ts-expect-error
+    pet.breed === 'lizard'
   }
 })
 
-export default init({
-  collections: {
-    person,
-    // pet,
-  }
-})
+export default init()
+
