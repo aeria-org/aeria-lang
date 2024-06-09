9979d0c8d4ba5b0087bcda3a61ea1bec2ff6e632
ringeringeraja <joaosan177@gmail.com>
2024-06-09 18:09:24 -0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 const eslintConfigAeria = require('eslint-config-aeria')
 eslintConfigAeria.languageOptions.parserOptions.project = ['./tsconfig.eslint.json']
+eslintConfigAeria.languageOptions.parserOptions.tsconfigRootDir = __dirname
+
 
 module.exports = [eslintConfigAeria]
