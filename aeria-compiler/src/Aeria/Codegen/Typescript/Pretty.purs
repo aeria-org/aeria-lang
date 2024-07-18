21e623c3c6cdabe253a1e1ddded843d89d818f1a
Gabriel Augusto <gabrielolivrp@gmail.com>
2024-07-17 21:16:56 -0300

    refactor(aeria-compiler): Pretty TypeScript

--- aeria-compiler/src/Aeria/Codegen/Typescript/Pretty.purs
+++ aeria-compiler/src/Aeria/Codegen/Typescript/Pretty.purs
@@ -12,46 +12,47 @@ ppTypescript (TsStatements stmts) =
   L.foldr (\s r -> ppStatement s <> "\n" <> r) "" stmts
 
 ppStatement :: TsStatement -> String
-ppStatement =
-  case _ of
-    ImportDeclaration specifiers ident ->
-      "import { " <> ppSpecifiers specifiers <> " } from \"" <> ppIdentifier ident <> "\""
-    VariableDeclaration ident type_ -> "const " <> ppIdentifier ident <> ": " <> ppTree type_
-    TypeAliasDeclaration ident type_ -> "type " <> ppIdentifier ident <> " = " <> ppTree type_
-    ExportDeclaration statement -> "export " <> ppStatement statement
-    DeclareDeclaration statement -> "declare " <> ppStatement statement
-    TSEmptyStatement -> ""
+ppStatement (ImportDeclaration specifiers ident) =
+  "import { " <> ppSpecifiers specifiers <> " } from \"" <> ppIdentifier ident <> "\""
+ppStatement (VariableDeclaration ident type_) =
+  "const " <> ppIdentifier ident <> ": " <> ppTree type_
+ppStatement (TypeAliasDeclaration ident type_) =
+  "type " <> ppIdentifier ident <> " = " <> ppTree type_
+ppStatement (ExportDeclaration statement) =
+  "export " <> ppStatement statement
+ppStatement (DeclareDeclaration statement) =
+  "declare " <> ppStatement statement
+ppStatement TSEmptyStatement = ""
 
 ppTree :: Tree -> String
-ppTree =
-  case _ of
-    Type tsType -> ppType tsType
-    Call ident args -> ppIdentifier ident <> "(" <> concatWith args ppTree <> ")"
-    Variable ident -> ppIdentifier ident
-    TypeQuery tree -> "typeof " <> ppTree tree
-    Extends tree1 tree2 -> ppTree tree1 <> " extends " <> ppTree tree2
-    Intersection tree1 tree2 -> ppTree tree1 <> " & " <> ppTree tree2
+ppTree (Type tsType) = ppType tsType
+ppTree (Call ident args) = ppIdentifier ident <> "(" <> concatWith args ppTree <> ")"
+ppTree (Variable ident) = ppIdentifier ident
+ppTree (TypeQuery tree) = "typeof " <> ppTree tree
+ppTree (Extends tree1 tree2) = ppTree tree1 <> " extends " <> ppTree tree2
+ppTree (Intersection tree1 tree2) = ppTree tree1 <> " & " <> ppTree tree2
 
 ppType :: TsType -> String
 ppType TypeAny = "any"
+ppType TypeBool = "boolean"
 ppType TypeString = "string"
 ppType TypeNumber = "number"
-ppType TypeBool = "boolean"
+ppType TypeLiteralNull = "null"
+ppType TypeLiteralUndefined = "undefined"
 ppType (TypeRaw raw) = raw
 ppType (TypeArray tsType) = ppType tsType <> "[]"
-ppType (TypeLiteralNull) = "null"
-ppType (TypeLiteralUndefined) = "undefined"
 ppType (TypeLiteralString value) = "\"" <> value <> "\""
 ppType (TypeLiteralBool value) = show value
 ppType (TypeLiteralNumber value) = show value
 ppType (TypeLiteralArray values) = "[" <> concatWith values ppTree <> "]"
 ppType (TypeVariable ident) = ppIdentifier ident
 ppType (TypeGeneric params tree) =
-  case tree of
-    Type (TypeFunction _ _) -> "<" <> concatWith params ppTree <> ">" <> ppTree tree
-    _ -> ppTree tree <> "<" <> concatWith params ppTree <> ">"
+  let paramsStr = "<" <> concatWith params ppTree <> ">"
+  in case tree of
+       Type (TypeFunction _ _) -> paramsStr <> ppTree tree
+       _ -> ppTree tree <> paramsStr
 ppType (TypeFunction params returnType) =
-  "(" <> concatWith params ((\(ident /\ tree) -> ppIdentifier ident <> ": " <> ppTree tree)) <> ") => " <> ppTree returnType
+  "(" <> concatWith params (\(ident /\ tree) -> ppIdentifier ident <> ": " <> ppTree tree) <> ") => " <> ppTree returnType
 ppType (TypeObject props) =
   "{" <> concatWith props (\(TypeObjectProperty ident tree) -> ppIdentifier ident <> ": " <> ppTree tree) <> "}"
 
