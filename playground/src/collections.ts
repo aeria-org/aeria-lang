b082bc4b3ca12552bb1bbb8fc3068629ef0f2f12
ringeringeraja <joaosan177@gmail.com>
2024-11-14 20:33:27 -0300

    ci: build playground

--- playground/src/collections.ts
+++ playground/src/collections.ts
@@ -1,1 +1,13 @@
-export * from '../.aeria/out/index.mjs'
+export * as collections from '../.aeria/out/collections/index.mjs'
+import { extendPetCollection } from '../.aeria/out/collections/pet.mjs'
+
+export const pet = extendPetCollection({
+  description: {
+    properties: {
+      is_docile: {
+        type: 'boolean',
+      },
+    },
+  },
+})
+
